2c58e420a5571bb21eb604c35808fb949ce9069f
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-12-17 14:19:06 +0100

    Remove NgxFlowUiService in demo

--- projects/ngx-flow-ui-demo/src/app/app.config.server.ts
+++ projects/ngx-flow-ui-demo/src/app/app.config.server.ts
@@ -1,10 +1,9 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { NgxFlowUiService } from '../../../ngx-flow-ui/src/public-api';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), NgxFlowUiService],
+  providers: [provideServerRendering()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
