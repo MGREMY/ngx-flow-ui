4bf32fc39a1695c55e93a25fb1ce4dec21958339
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-12-17 03:36:59 +0100

    Add library

---
+++ projects/ngx-flow-ui/src/lib/ngx-flow-ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flow-ui-ngx-flow-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ngx-flow-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxFlowUiComponent {
+
+}
---
+++ projects/ngx-flow-ui/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-flow-ui
+ */
+
+export * from './lib/ngx-flow-ui.service';
+export * from './lib/ngx-flow-ui.component';
