7d26ffdaee35499cb28fe67a3e28bc260dbe2476
MGREMY <126514209+MGREMY@users.noreply.github.com>
2024-01-04 00:09:59 +0100

    Remove background from outline badge (#47)
    <!-- @format -->
    
    ## Describe your changes
    Remove background from outline badge
    
    ## Github linked issue
    
    > Closes #46 
    
    ## Checklist before requesting a review
    
    - [x] I have performed a self-review of my code
    - [x] If it is a core feature, I have added thorough tests.

--- projects/ngx-flow-ui/src/lib/components/ngx-flow-ui-badge/ngx-flow-ui-badge.properties.ts
+++ projects/ngx-flow-ui/src/lib/components/ngx-flow-ui-badge/ngx-flow-ui-badge.properties.ts
@@ -29,10 +29,10 @@ export class NgxFlowUiBadgeProperties implements IComponentProperties {
       yellow: ' bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-800',
     },
     outline: {
-      blue: ' bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-800',
-      red: ' bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-800',
-      green: ' bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-800',
-      yellow: ' bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-800',
+      blue: ' text-blue-800 dark:text-blue-800',
+      red: ' text-red-800 dark:text-red-800',
+      green: ' text-green-800 dark:text-green-800',
+      yellow: ' text-yellow-800 dark:text-yellow-800',
     },
   };
 
